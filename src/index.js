f6909066b46b1af97490dce5acd1b1b9f09bd10e
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-09-03 13:32:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const PORT = 3000; 
 
+app.get("/", ()=>{
+
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 });
